3f2fdc99c5ef580997ed0f0a29d13ce2fd51842d
Albert Saucedo <albert@Alberts-Mac-mini.local>
2014-12-01 20:30:08 -0600

    Initial Commit

---
+++ Productivity Timer Apple Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Productivity Timer Apple Watch
+//
+//  Created by Albert Saucedo on 12/1/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
